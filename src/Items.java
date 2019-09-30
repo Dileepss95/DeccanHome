64bbbbde0bf34c1956f1cfa8c2e06d2fa9e16172
Dileep <ndileepssgre@gmail.com>
2019-09-29 20:21:43 -0500

    made some changes

--- src/Items.java
+++ src/Items.java
@@ -3,7 +3,7 @@ public class Items {
 	private int quantity;
 	private int price;
 	Items(){
-		System.out.println("You have started again but you need to finish this time");
+		System.out.println("You have started again you almost did it watch about corner cases");
 	}
 
 }
